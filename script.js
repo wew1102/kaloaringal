ea7735b6772736f52d0189e6fabc47cc4d6e00f0
Alex <alexyonjanlama@gmail.com>
2024-11-15 08:10:04 +0545

    h

--- script.js
+++ script.js
@@ -1,36 +1,137 @@
-const jokeEl = document.getElementById('joke')
-const jokeBtn = document.getElementById('jokeBtn')
+var playing = false;
+var score;
+var action;
+var timeremaining;
+var correctans;
+var arrAns = [];
+document.getElementById("startreset").onclick = function() {
+    if (playing == true) {
+        location.reload();
 
-jokeBtn.addEventListener('click', generateJoke)
+    } else {
+        score = 0;
+        playing = true;
+        document.getElementById("timeremaining").style.display = "block";
 
-generateJoke()
+        document.getElementById("scorevalue").innerHTML = score;
+        document.getElementById("startreset").innerHTML = "reset";
+        timeremaining = 60;
+        count();
+        document.getElementById("timeremainingvalue").innerHTML = timeremaining;
+        hide("gameOver");
 
-// USING ASYNC/AWAIT
-async function generateJoke() {
-  const config = {
-    headers: {
-      Accept: 'application/json',
-    },
-  }
+        hide("correct");
+        hide("wrong");
+        generateQA();
 
-  const res = await fetch('https://icanhazdadjoke.com', config)
 
-  const data = await res.json()
+        for (var i = 1; i < 5; i++) {
+            document.getElementById("box" + i).onclick = function() {
 
-  jokeEl.innerHTML = data.joke
+                if (this.innerHTML == correctans) {
+
+                    show("correct");
+                    hide("wrong");
+                    score++;
+                    document.getElementById("scorevalue").innerHTML = score;
+                    setTimeout(function() {
+                        hide("correct");
+
+                    }, 1050)
+
+                    generateQA();
+
+
+                } else {
+                    hide("correct");
+                    show("wrong");
+                    setTimeout(function() {
+                        hide("wrong");
+
+                    }, 1050)
+
+                }
+
+            }
+        }
+
+    }
+
 }
 
-// USING .then()
-// function generateJoke() {
-//   const config = {
-//     headers: {
-//       Accept: 'application/json',
-//     },
-//   }
+function count() {
+    action = setInterval(
 
-//   fetch('https://icanhazdadjoke.com', config)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       jokeEl.innerHTML = data.joke
-//     })
-// }
+        function() {
+
+            timeremaining -= 1;
+            document.getElementById("timeremainingvalue").innerHTML = timeremaining;
+            if (timeremaining == 0) {
+                clear();
+                hide("timeremaining");
+
+                document.getElementById("startreset").innerHTML = "Start Game";
+                show("gameOver");
+                document.getElementById("gameOver").innerHTML = "<p> Game Over <p> <p>Your Score is " + score + "</p>";
+                playing = false;
+            }
+
+
+        }, 1000);
+}
+
+function hide(id) {
+
+    document.getElementById(id).style.display = "none";
+}
+
+function show(id) {
+
+    document.getElementById(id).style.display = "block";
+}
+
+function clear() {
+
+    clearInterval(action);
+
+}
+
+function generateQA() {
+    var arrAns = [];
+    do {
+        var x = 1 + Math.round(9 * Math.random());
+        var y = 1 + Math.round(9 * Math.random());
+        correctans = x * y;
+    }
+    while (arrAns.indexOf(correctans) > -1)
+    arrAns.push(correctans);
+    var correctpos = 1 + Math.round(3 * Math.random());
+    document.getElementById("question").innerHTML = x + "x" + y;
+    document.getElementById("box" + correctpos).innerHTML = correctans;
+    var wrongans;
+    var arr2 = [correctans];
+    for (var i = 1; i < 5; i++) {
+        if (i != correctpos) {
+            do { wrongans = 1 + Math.round(9 * Math.random()) * 1 + Math.round(9 * Math.random()); }
+            while (arr2.indexOf(wrongans) > -1)
+            document.getElementById("box" + i).innerHTML = wrongans;
+
+            arr2.push(wrongans);
+
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
